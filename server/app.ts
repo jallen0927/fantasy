de211d82aee40ab447fab6f51eef4342f7f6b9da
Shawn <jallen0927@gmail.com>
2016-11-03 23:27:30 +1300

    Basic implementation of fetching and writing.

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as path from 'path';
 import errorHandler = require("errorhandler");
 import methodOverride = require("method-override");
+import { MongoClient }  from 'mongodb';
 
 import * as routes from "./routes/index";
 
@@ -10,15 +11,23 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-var env = process.env.NODE_ENV || 'development';
+let env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
     app.use(errorHandler());
 }
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
-    console.log("Demo Express server listening on port %d in %s mode", 3000, app.settings.env);
+let url = 'mongodb://localhost:27017/fantasy';
+
+MongoClient.connect(url, (err, db) => {
+    if(err) {
+        console.error(err);
+    }
+    app.locals.db = db;
+    app.listen(3000, function(){
+        console.log("Demo Express server listening on port %d in %s mode", 3000, app.settings.env);
+    });
 });
 
 export default app;
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,52 @@
-import express = require("express");
+import * as http from 'http';
+import * as express from 'express';
+import app from '../app';
+import {error} from "util";
+
+let options = {
+    host: 'www.newzealand.com',
+    path: '/api/rest/v1/deliver/listings?tag=auckland&level=full&maxrows=10&skip=0',
+    headers: {
+    }
+};
+
+const fetchRecords = function () {
+
+    return new Promise((resolve, reject) => {
+        http.get(options, (data) => {
+            let content = '';
+            data.setEncoding('utf8');
+            data.on('data', (chunk) => content += chunk);
+            data.on('end', () => {
+                resolve(JSON.parse(content));
+            });
+        });
+    });
+};
+
+const writeRecords = function(collectionName, data) {
+    let collection = app.locals.db.collection(collectionName);
+    data.forEach((item) => {
+        collection.findOne({unique_id: item.unique_id}, (err, result) => {
+            collection = app.locals.db.collection('test');
+            if(err) return console.error(err);
+            if(!result) {
+                collection.insertOne(item);
+            }
+        });
+    });
+};
+
 
 export function index(req: express.Request, res: express.Response) {
-    res.render('index', { title: 'fantasy' });
-};
+    fetchRecords().then(
+        (data) => {
+            let totalNumber = data.meta.total_items;
+            console.log(totalNumber);
+            writeRecords('test', data.items);
+        }
+    ).then(
+        () => res.end('finished')
+    ).catch((error) => console.error(error));
+    // res.render('index', { title: 'fantasy' });
+}
